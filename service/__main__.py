767ecc20033ce85825b1b06ef8bd44eca810325f
Ismail <i.nazirov@gt-sot.ru>
2024-06-04 23:29:29 +0300

    improve code style

--- service/__main__.py
+++ service/__main__.py
@@ -1,6 +1,12 @@
 import asyncio
 import asyncpg
-from db_work import get_all_documents, get_document, insert_new_katalog,insert_new_document, get_katalog_info
+from db_work import (
+    get_all_documents,
+    get_document,
+    insert_new_katalog,
+    insert_new_document,
+    get_katalog_info,
+)
 from quart import Quart, request
 from quart_auth import basic_auth_required, QuartAuth
 import secrets
@@ -9,60 +15,68 @@ import yaml
 app = Quart(__name__)
 app.config["QUART_AUTH_BASIC_USERNAME"] = "admin"
 app.config["QUART_AUTH_BASIC_PASSWORD"] = "admin"
-app.secret_key = secrets.token_urlsafe(16)  
+app.secret_key = secrets.token_urlsafe(16)
 
 QuartAuth(app)
 
 
 async def connection():
-    conn=await asyncpg.connect(user='admin', password='admin',
-                                 database='katalog', host='postgres', port=5432)
+    conn = await asyncpg.connect(
+        user="admin", password="admin", database="katalog", host="postgres", port=5432
+    )
     return conn
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 async def all_info():
-    conn=await connection()
-    result=await get_all_documents(conn)
+    conn = await connection()
+    result = await get_all_documents(conn)
     return yaml.dump(result)
 
-@app.route('/<path:path>', methods=['GET'])
+
+@app.route("/<path:path>", methods=["GET"])
 async def document(path):
-    conn=await connection()
-    split_path=path.split('/')
-    last_info=split_path[-1]
-    last_info_split=last_info.split("=")
-    if len(last_info_split)==2 and last_info_split[0]=='document':
-        document_name=last_info_split[1]
-        result=await get_document(conn, path, document_name)
+    conn = await connection()
+    split_path = path.split("/")
+    last_info = split_path[-1]
+    last_info_split = last_info.split("=")
+    if len(last_info_split) == 2 and last_info_split[0] == "document":
+        document_name = last_info_split[1]
+        result = await get_document(conn, path, document_name)
     else:
-        result=await get_katalog_info(conn, path)
-        result=yaml.dump(result)
+        result = await get_katalog_info(conn, path)
+        result = yaml.dump(result)
     return result
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 @basic_auth_required()
 async def insert_new_data():
-    conn=await connection()
-    data=await request.get_data()
-    str_data=data.decode("utf-8")
-    if str_data[0]!='[' and str_data[-1]!=']':
-        return 'wrong request'
-    str_data=str_data[1:-1]
-    split_data=str_data.split(',')
-    if split_data[0]=='document' and len(split_data)==6 and '' not in split_data:
-        katalog_path=split_data[1]
-        document_name=split_data[2]
-        document_format=split_data[3]
-        document_size=split_data[4]
-        document_hash=split_data[5]
-        result=await insert_new_document(conn,katalog_path,(document_name,document_format,document_size,document_hash))
-    elif split_data[0]=='katalog' and len(split_data)==3 and '' not in split_data:
-        katalog_path=split_data[1]
-        new_katalog_name=split_data[2]
-        result=await insert_new_katalog(conn, katalog_path, new_katalog_name)
+    conn = await connection()
+    data = await request.get_data()
+    str_data = data.decode("utf-8")
+    if str_data[0] != "[" and str_data[-1] != "]":
+        return "wrong request"
+    str_data = str_data[1:-1]
+    split_data = str_data.split(",")
+    if split_data[0] == "document" and len(split_data) == 6 and "" not in split_data:
+        katalog_path = split_data[1]
+        document_name = split_data[2]
+        document_format = split_data[3]
+        document_size = split_data[4]
+        document_hash = split_data[5]
+        result = await insert_new_document(
+            conn,
+            katalog_path,
+            (document_name, document_format, document_size, document_hash),
+        )
+    elif split_data[0] == "katalog" and len(split_data) == 3 and "" not in split_data:
+        katalog_path = split_data[1]
+        new_katalog_name = split_data[2]
+        result = await insert_new_katalog(conn, katalog_path, new_katalog_name)
     else:
-        return 'wrong request'
+        return "wrong request"
     return result
 
 
-app.run(host='0.0.0.0')
+app.run(host="0.0.0.0")
--- service/db_work.py
+++ service/db_work.py
@@ -1,119 +1,140 @@
 import asyncio
 
+
 async def get_all_documents(conn):
-    all_katalog_info=await conn.fetch(
-        'SELECT id,name,parent_id FROM katalog'
+    all_katalog_info = await conn.fetch("SELECT id,name,parent_id FROM katalog")
+    all_documents = await conn.fetch(
+        "SELECT id,name,format,size,hash,parent_katalog_id FROM documents"
     )
-    all_documents=await conn.fetch(
-        'SELECT id,name,format,size,hash,parent_katalog_id FROM documents'
-    )
-    katalogs={}
+    katalogs = {}
     for katalog in all_katalog_info:
-        id=katalog[0]
-        name=katalog[1]
-        parent_id=katalog[2]
-        katalogs[id]={'name':name, 'parent_id':parent_id, 'inside': []}
+        id = katalog[0]
+        name = katalog[1]
+        parent_id = katalog[2]
+        katalogs[id] = {"name": name, "parent_id": parent_id, "inside": []}
     for document in all_documents:
-        id=document[0]
-        name=document[1]
-        format=document[2]
-        size=document[3]
-        hash=document[4]
-        katalog_parent_id=document[5]
-        katalogs[katalog_parent_id]['inside'].append({'name':name, 'format':format, 'size':size, 'hash':hash})
-    finish_katalog=[]
+        id = document[0]
+        name = document[1]
+        format = document[2]
+        size = document[3]
+        hash = document[4]
+        katalog_parent_id = document[5]
+        katalogs[katalog_parent_id]["inside"].append(
+            {"name": name, "format": format, "size": size, "hash": hash}
+        )
+    finish_katalog = []
     for id in katalogs:
-        katalog=katalogs[id]
-        parent_id=katalog['parent_id']
-        if parent_id==0:
+        katalog = katalogs[id]
+        parent_id = katalog["parent_id"]
+        if parent_id == 0:
             continue
-        katalogs[parent_id]['inside'].append({'name':katalog['name'], 'inside':katalog['inside']})
+        katalogs[parent_id]["inside"].append(
+            {"name": katalog["name"], "inside": katalog["inside"]}
+        )
     for id in katalogs:
-        katalog=katalogs[id]
-        if katalog['parent_id']==0:
-            finish_katalog.append({'name':katalog['name'], 'inside':katalog['inside']})
+        katalog = katalogs[id]
+        if katalog["parent_id"] == 0:
+            finish_katalog.append(
+                {"name": katalog["name"], "inside": katalog["inside"]}
+            )
     return finish_katalog
 
-async def get_document(conn,katalog_path,document_name):
-    split_katalog_path=katalog_path.split("/")[:-1]
-    parent_id=0
+
+async def get_document(conn, katalog_path, document_name):
+    split_katalog_path = katalog_path.split("/")[:-1]
+    parent_id = 0
     for katalog_name in split_katalog_path:
-        values=await conn.fetch(
-            '''SELECT id FROM katalog where name='%s' and parent_id='%s' '''%(katalog_name,parent_id)
+        values = await conn.fetch(
+            """SELECT id FROM katalog where name='%s' and parent_id='%s' """
+            % (katalog_name, parent_id)
         )
-        if values==[]:
+        if values == []:
             return "wrong path"
-        parent_id=values[0][0]
-    document_data=await conn.fetch(
-            '''SELECT name,format,size,hash FROM documents where name='%s' and parent_katalog_id='%s' '''%(document_name,parent_id)
-        )
-    if len(document_data)==0:
+        parent_id = values[0][0]
+    document_data = await conn.fetch(
+        """SELECT name,format,size,hash FROM documents where name='%s' and parent_katalog_id='%s' """
+        % (document_name, parent_id)
+    )
+    if len(document_data) == 0:
         return "Nothing found. Maybe wrong document name"
-    document_info={'name':document_data[0][0], 'format':document_data[0][1], 'size':document_data[0][2], 'hash':document_data[0][3]}
+    document_info = {
+        "name": document_data[0][0],
+        "format": document_data[0][1],
+        "size": document_data[0][2],
+        "hash": document_data[0][3],
+    }
     return document_info
 
-async def get_katalog_info(conn,katalog_path):
-    split_katalog_path=katalog_path.split("/")
-    if split_katalog_path[-1]=="":
-        split_katalog_path=split_katalog_path[:-1]
-    cur_layer_info= await get_all_documents(conn)
+
+async def get_katalog_info(conn, katalog_path):
+    split_katalog_path = katalog_path.split("/")
+    if split_katalog_path[-1] == "":
+        split_katalog_path = split_katalog_path[:-1]
+    cur_layer_info = await get_all_documents(conn)
     for katalog_name in split_katalog_path:
-        flag=False
-        new_layer=None
+        flag = False
+        new_layer = None
         for cur_layer_katalog in cur_layer_info:
-            if cur_layer_katalog['name']==katalog_name:
-                new_layer=cur_layer_katalog['inside']
-                flag=True
-        if flag==False:
-            return 'Wrong path'
+            if cur_layer_katalog["name"] == katalog_name:
+                new_layer = cur_layer_katalog["inside"]
+                flag = True
+        if flag == False:
+            return "Wrong path"
         else:
-            cur_layer_info=new_layer
+            cur_layer_info = new_layer
     return cur_layer_info
-        
-    
+
 
 async def insert_new_katalog(conn, katalog_path, new_katalog_name):
-    split_katalog_path=katalog_path.split("/")
-    parent_id=0
+    split_katalog_path = katalog_path.split("/")
+    parent_id = 0
     for katalog_name in split_katalog_path:
-        values=await conn.fetch(
-            '''SELECT id FROM katalog where name='%s' and parent_id='%s' '''%(katalog_name,parent_id)
+        values = await conn.fetch(
+            """SELECT id FROM katalog where name='%s' and parent_id='%s' """
+            % (katalog_name, parent_id)
         )
-        if values==[]:
+        if values == []:
             return "wrong path"
-        parent_id=values[0][0]
-    already_exist_katalog=await conn.fetch(
-            '''SELECT id FROM katalog where name='%s' and parent_id='%s' '''%(new_katalog_name,parent_id)
-        )
-    if len(already_exist_katalog)>0:
-        return 'already exist'
-    a=await conn.execute(
-            '''INSERT INTO public.katalog(name, parent_id)
-	        VALUES ('%s', '%s')'''%(new_katalog_name,parent_id)
-        )
-    return 'done'
+        parent_id = values[0][0]
+    already_exist_katalog = await conn.fetch(
+        """SELECT id FROM katalog where name='%s' and parent_id='%s' """
+        % (new_katalog_name, parent_id)
+    )
+    if len(already_exist_katalog) > 0:
+        return "already exist"
+    a = await conn.execute(
+        """INSERT INTO public.katalog(name, parent_id)
+	        VALUES ('%s', '%s')"""
+        % (new_katalog_name, parent_id)
+    )
+    return "done"
+
 
 async def insert_new_document(conn, katalog_path, document_info):
-    split_katalog_path=katalog_path.split("/")
-    parent_id=0
-    document_name=document_info[0]
-    document_format=document_info[1]
-    document_size=document_info[2]
-    document_hash=document_info[3]
+    split_katalog_path = katalog_path.split("/")
+    parent_id = 0
+    document_name = document_info[0]
+    document_format = document_info[1]
+    document_size = document_info[2]
+    document_hash = document_info[3]
     for katalog_name in split_katalog_path:
-        values=await conn.fetch(
-            '''SELECT id FROM katalog where name='%s' and parent_id='%s' '''%(katalog_name,parent_id)
+        values = await conn.fetch(
+            """SELECT id FROM katalog where name='%s' and parent_id='%s' """
+            % (katalog_name, parent_id)
         )
-        if values==[]:
+        if values == []:
             return "wrong path"
-        parent_id=values[0][0]
-    already_exist_document=await conn.fetch(
-            '''SELECT id FROM documents where name='%s' and parent_katalog_id='%s' '''%(document_name,parent_id)
-        )
-    if len(already_exist_document)>0:
-        return 'already exist'
-    a=await conn.execute('''INSERT INTO documents(
+        parent_id = values[0][0]
+    already_exist_document = await conn.fetch(
+        """SELECT id FROM documents where name='%s' and parent_katalog_id='%s' """
+        % (document_name, parent_id)
+    )
+    if len(already_exist_document) > 0:
+        return "already exist"
+    await conn.execute(
+        """INSERT INTO documents(
             name, format, size, hash, parent_katalog_id
-            ) VALUES ('%s', '%s', '%s','%s','%s')'''%(document_name,document_format,document_size,document_hash,parent_id)
-        )
-    return 'done'
+            ) VALUES ('%s', '%s', '%s','%s','%s')"""
+        % (document_name, document_format, document_size, document_hash, parent_id)
+    )
+    return "done"
